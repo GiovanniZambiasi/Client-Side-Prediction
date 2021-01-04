624209d4e1509b3b50a0bef31ca6f5632e89b94b
GiovanniZambiasi <giovanni.zambiasi@gmail.com>
2021-01-04 16:07:55 -0300

    Remove comments

--- Assets/_GiosNetworking/Scripts/NetworkedClient/Rigidbody/NetworkedRigidbody.cs
+++ Assets/_GiosNetworking/Scripts/NetworkedClient/Rigidbody/NetworkedRigidbody.cs
@@ -32,19 +32,11 @@ namespace ClientSidePrediction.RB
 
         public override void ProcessInput(RigidbodyInput input)
         {
-            //_rigidbody.constraints = RigidbodyConstraints.None;
-            
             var __force = new Vector3(input.movement.x, 0f, input.movement.y);
             __force *= _speed * input.deltaTime;
             _rigidbody.AddForce(__force, ForceMode.Impulse);
             
             _physicsScene.Simulate(input.deltaTime);
-
-            //Debug.Log($"Vel before. Kinematic false: {_rigidbody.velocity.ToString()}");
-            
-            //_rigidbody.constraints = RigidbodyConstraints.FreezeAll;
-            
-            //Debug.Log($"Vel after Kinematic true: {_rigidbody.velocity.ToString()}");
         }
 
         protected override RigidbodyState RecordState(uint lastProcessedInputTick)
